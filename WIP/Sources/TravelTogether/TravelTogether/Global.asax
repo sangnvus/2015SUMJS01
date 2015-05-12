a93ff53695ae9eb5538e114abf08b8d18e400284
Duc Filan <ducfilan@gmail.com>
2015-05-12 12:15:32 +0700

    Source code structure

---
+++ WIP/Sources/TravelTogether/TravelTogether/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TravelTogether.MvcApplication" Language="C#" %>
---
+++ WIP/Sources/TravelTogether/TravelTogether/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelTogether.Startup))]
+namespace TravelTogether
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
