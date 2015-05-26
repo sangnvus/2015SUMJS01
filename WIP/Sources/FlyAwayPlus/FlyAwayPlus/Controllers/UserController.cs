383dd98d3b6b0bb0862417ebac08e5cb65c36aa5
Nguyễn Hoàng <hoangnmse02819@fpt.edu.vn>
2015-05-26 22:46:36 +0700

    User detail
    User detail

---
+++ WIP/Sources/FlyAwayPlus/FlyAwayPlus/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlyAwayPlus.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
