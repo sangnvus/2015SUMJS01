d240f4127ab95e22190a0768b02780bc73372838
haidt4 <haidtse02856@fpt.edu.vn>
2015-06-02 22:02:34 +0700

    Search result prototype
    Search result prototype

---
+++ WIP/Sources/FlyAwayPlus/FlyAwayPlus/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlyAwayPlus.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult SearchDetail()
+        {
+            return View();
+        }
+	}
+}
