aba7db60d990d5137a84ffa4a8619de88ccdaaac
haidt4 <haidtse02856@fpt.edu.vn>
2015-07-07 20:44:15 +0700

    Update source code
    Update source code

--- WIP/Sources/FlyAwayPlus/FlyAwayPlus/Controllers/FapAccountController.cs
+++ WIP/Sources/FlyAwayPlus/FlyAwayPlus/Controllers/FapAccountController.cs
@@ -7,11 +7,18 @@ using Facebook;
 using System.Configuration;
 using Newtonsoft.Json;
 using System.IO;
+using FlyAwayPlus.Models;
+using System.Web.Security;
+using System.Net;
+using Neo4jClient;
+using FlyAwayPlus.Helpers;
 
 namespace FlyAwayPlus.Controllers
 {
     public class FapAccountController : Controller
     {
+        private FlyAwayPlusEntities fap = new FlyAwayPlusEntities();
+
         //
         // GET: /FapAccount/
         public ActionResult Index()
@@ -19,11 +26,111 @@ namespace FlyAwayPlus.Controllers
             return View();
         }
 
-        public ActionResult Register()
+        public ActionResult Login()
         {
-            return View();
+            string email = Request["username"];
+            string password = Request["password"];
+
+            List<User> listUser = null;
+
+            User user = new User();
+            user.email = email;
+            user.typeID = 0;
+
+            UserDetail userDetail = new UserDetail();
+            
+            listUser = fap.Users.Where(u => u.typeID == 0 && u.email.Equals(email)).ToList();
+            if (listUser.Count == 0)
+            {
+                /*
+                 * Doing something
+                 */
+            }
+            else
+            {
+                user = listUser.First();
+                userDetail.userID = user.userID;
+
+                userDetail = fap.UserDetails.Find(userDetail.userID);
+            }
+
+            if (userDetail.password.Equals(password))
+            {
+                Session["authenicated"] = true;
+                Session["username"] = userDetail.firstName + " " + userDetail.lastName;
+                Session["user"] = user;
+            }
+            
+            return RedirectToAction("Index", "Home");
         }
 
+
+        public ActionResult Register()
+        {
+            string firstName = Request["first_name"];
+            string lastName = Request["last_name"];
+            string phoneNumber = Request["phone_number"];
+            string dob = Request["dob"];
+            string gender = Request["gender-options"];
+            string address = Request["address"];
+            string email = Request["email"];
+            string password = Request["password"];
+            string password_confirm = Request["password_confirmation"];
+
+            User newUser = new User();
+            newUser.typeID = 0; // Fap account
+            newUser.email = email;
+
+            UserDetail newUserDetail = new UserDetail();
+            newUserDetail.address = address;
+            newUserDetail.dateJoined = DateTime.Now;
+            newUserDetail.dateOfBirth = DateTime.Now;
+            newUserDetail.firstName = firstName;
+            newUserDetail.lastName = lastName;
+            newUserDetail.gender = gender;
+            newUserDetail.phoneNumber = phoneNumber;
+            newUserDetail.status = "active";
+            newUserDetail.password = password;
+
+
+            List<User> listUser = null;
+
+            listUser = fap.Users.Where(user => user.typeID == 0 && user.email.Equals(email)).ToList();
+            if (listUser.Count == 0)
+            {
+                fap.Users.Add(newUser);
+                fap.SaveChanges();
+
+                newUser = fap.Users.Where(user => user.typeID == 0 && user.email.Equals(email)).ToList().First();
+
+                newUserDetail.userID = newUser.userID;
+
+                fap.UserDetails.Add(newUserDetail);
+                fap.SaveChanges();
+
+                /*
+                *  Insert into Graph DB 
+                */
+                GraphDatabaseHelpers.insertUser(newUser);
+            }
+            else
+            {
+                newUser = listUser.First();
+            }
+
+
+            // Set the auth cookie
+            Session["authenicated"] = true;
+            Session["username"] = newUserDetail.firstName + " " + newUserDetail.lastName;
+            Session["user"] = newUser;
+
+            //FormsAuthentication.SetAuthCookie(email, false);
+            //SessionHelper.RenewCurrentUser();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        /*
         [HttpPost]
         public ActionResult Register(HttpPostedFileBase file)
         {
@@ -36,8 +143,10 @@ namespace FlyAwayPlus.Controllers
 
             return RedirectToAction("Index");
         }
+        */
         public ActionResult AuthenFacebook()
         {
+
             var fb = new FacebookClient();
             var loginUrl = fb.GetLoginUrl(new
             {
@@ -47,8 +156,10 @@ namespace FlyAwayPlus.Controllers
                 //"23aacf540a138ebcad87f1ee1825fc05",
                 redirect_uri = RedirectUri.AbsoluteUri,
                 response_type = "code",
+                
                 scope = "email" // Add other permissions as needed
             });
+
 
             return Redirect(loginUrl.AbsoluteUri);
         }
@@ -73,16 +184,60 @@ namespace FlyAwayPlus.Controllers
             // we can make requests on behalf of the user
             fb.AccessToken = accessToken;
 
+
             // Get the user's information
-            dynamic me = fb.Get("me?fields=first_name,last_name,id,email");
-            dynamic tem = fb.Get("/me");
-
+            dynamic me = fb.Get("/me");
             string email = me.email;
-            
-            // Set the auth cookie
-            //FormsAuthentication.SetAuthCookie(email, false);
 
-            return RedirectToAction("Register", "FapAccount");
+            User newUser = new User();
+            newUser.typeID = 1; // Facebook account
+            newUser.email = email;
+
+            UserDetail newUserDetail = new UserDetail();
+            newUserDetail.address = me.adress;
+            newUserDetail.dateJoined = DateTime.Now;
+            newUserDetail.dateOfBirth = me.date_of_birth;
+            newUserDetail.firstName = me.first_name;
+            newUserDetail.lastName = me.last_name;
+            newUserDetail.gender = me.gender;
+            newUserDetail.phoneNumber = me.phone_number;
+            newUserDetail.status = "active";
+
+            List<User> listUser = null;
+
+            listUser = fap.Users.Where(user => user.typeID == 1 && user.email.Equals(email)).ToList();
+            if (listUser.Count == 0)
+            {
+                fap.Users.Add(newUser);
+                fap.SaveChanges();
+
+                newUser = fap.Users.Where(user => user.typeID == 1 && user.email.Equals(email)).ToList().First();
+
+                newUserDetail.userID = newUser.userID;
+
+                fap.UserDetails.Add(newUserDetail);
+                fap.SaveChanges();
+
+                /*
+                 *  Insert into Graph DB 
+                 */
+                GraphDatabaseHelpers.insertUser(newUser);
+                
+            }
+            else
+            {
+                newUser = listUser.First();
+            }
+
+            // Set the auth cookie
+            Session["authenicated"] = true;
+            Session["username"] = newUserDetail.firstName + " " + newUserDetail.lastName;
+            Session["user"] = newUser;
+
+            //FormsAuthentication.SetAuthCookie(email, false);
+            //SessionHelper.RenewCurrentUser();
+
+            return RedirectToAction("Index", "Home");
         }
 
         private Uri RedirectUri
---
+++ WIP/Sources/FlyAwayPlus/FlyAwayPlus/Helpers/GraphDatabaseHelpers.cs
@@ -0,0 +1,25 @@
+﻿using FlyAwayPlus.Models;
+using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace FlyAwayPlus.Helpers
+{
+    public class GraphDatabaseHelpers
+    {
+        private static GraphClient client = new GraphClient(new Uri(ConfigurationManager.AppSettings["dbGraphUri"]));
+
+        public static void insertUser(User user)
+        {
+            client.Connect();
+            var userRef = client.Cypher.
+                        Create("(user:user {userDetail})").
+                        WithParam("userDetail", user)
+                        .Return<User> ("user")
+                        .Results.Single();
+        }
+    }
+}
