93b7292d5e774680b55336b419148b15c566fa23
Nguyễn Hoàng <hoangnmse02819@fpt.edu.vn>
2015-08-04 00:03:38 +0700

    Add AdminController & list all user
    Add AdminController & list all user

---
+++ WIP/Sources/FlyAwayPlus/FlyAwayPlus/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlyAwayPlus.Helpers;
+using FlyAwayPlus.Models;
+
+namespace FlyAwayPlus.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            List<User> listAllUsers = new List<User>();
+            listAllUsers = GraphDatabaseHelpers.Instance.ListAllUsers();
+            ViewData["listAllUsers"] = listAllUsers;
+            return View();
+        }
+	}
+}
